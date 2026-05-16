f2c95e62f7318fc4e7a8d47a428ec2f4c577dd0c
Leo <combatverses@gmail.com>
2026-05-17 00:22:46 +0100

    remove session provider

--- app/SessionProvider.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import { SessionProvider as Provider } from 'next-auth/react'
-
-export default function SessionProvider({ children, session }: any) {
-  return <Provider session={session}>{children}</Provider>
-}
