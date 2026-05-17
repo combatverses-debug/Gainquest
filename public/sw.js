fa16514280f852631f83b7f866b75cb1b7e84cc6
Leo <combatverses@gmail.com>
2026-05-17 16:27:56 +0100

    fix service worker api interception

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
 self.addEventListener('install', () => self.skipWaiting())
 self.addEventListener('activate', () => self.clients.claim())
 self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/api/')) return
   event.respondWith(fetch(event.request).catch(() => caches.match(event.request)))
 })
