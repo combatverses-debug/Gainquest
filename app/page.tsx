c38049978f41cfcc6327007213d1e7183db360fe
Leo <combatverses@gmail.com>
2026-05-23 15:59:21 +0100

    move strava badge above sync button

--- app/page.tsx
+++ app/page.tsx
@@ -170,8 +170,9 @@ export default function Home() {
   <div style={{ fontSize: 10, color: "#4a3d6b", letterSpacing: "0.1em", textTransform: "uppercase" as const, marginBottom: 8 }}>The Ironveil Realm</div>
   <img src="/Gainquest-trans-logo.png" style={{ height: 100, objectFit: "contain", marginBottom: 10 }} alt="Gainquest" />
   <div style={{ paddingBottom: 12 }}>
-   <button style={s.syncBtn} onClick={syncActivities} disabled={syncing}>
     <img src="/powered-by-strava.png" style={{ height: 10, objectFit: "contain", opacity: 0.5, marginTop: 4 }} alt="Powered by Strava" />
+   <button style={s.syncBtn} onClick={syncActivities} disabled={syncing}>
+    
   {syncing ? "Syncing..." : "Sync Strava"}
 </button>
 
